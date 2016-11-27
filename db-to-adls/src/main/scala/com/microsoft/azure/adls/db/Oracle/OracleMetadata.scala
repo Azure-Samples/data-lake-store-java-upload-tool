982d8667d8f1bb869860c21f83512935b78df795
gandhinath <gandhinaths@gmail.com>
2016-11-27 12:47:56 -0800

    Made minor fixes - string concat - string upper case conversion to be compatible with Oracle

--- db-to-adls/src/main/scala/com/microsoft/azure/adls/db/Oracle/OracleMetadata.scala
+++ db-to-adls/src/main/scala/com/microsoft/azure/adls/db/Oracle/OracleMetadata.scala
@@ -25,19 +25,19 @@ trait OracleMetadata extends Metadata {
          | T.TABLE_NAME = P.TABLE_NAME
          | LEFT OUTER JOIN ALL_TAB_SUBPARTITIONS SP ON
          | P.TABLE_NAME = SP.TABLE_NAME and P.PARTITION_NAME = SP.PARTITION_NAME
-         | WHERE T.TABLE_NAME IN (${tables map (table => s"'$table'") mkString ", "})
+         | WHERE T.TABLE_NAME IN (${tables map (table => s"'${table.toUpperCase}'") mkString ", "})
        """.stripMargin
     if (partitions.nonEmpty) {
       builder ++= " AND( "
       builder ++= partitions.foldLeft(new StringBuilder) {
-        (sb, s) => sb append s"OR P.PARTITION_NAME LIKE %$s% "
+        (sb, s) => sb append s"OR P.PARTITION_NAME LIKE '%${s.toUpperCase}%' "
       }.delete(0, 3).toString
       builder ++= ")"
     }
     if (subPartitions.nonEmpty) {
       builder ++= " AND( "
-      builder ++= partitions.foldLeft(new StringBuilder) {
-        (sb, s) => sb append s"OR SP.SUBPARTITION_NAME LIKE %$s% "
+      builder ++= subPartitions.foldLeft(new StringBuilder) {
+        (sb, s) => sb append s"OR SP.SUBPARTITION_NAME LIKE '%${s.toUpperCase}%' "
       }.delete(0, 3).toString
       builder ++= ")"
     }
